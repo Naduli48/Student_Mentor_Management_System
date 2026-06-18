94d4df29626b560afad72b4fdf2f523b5e2158d9
Naduli48 <naduli.20232165@iit.ac.lk>
2026-06-18 17:48:20 +0530

    Class updated

--- src/User.java
+++ src/User.java
@@ -1,2 +1,26 @@
-public class User {
+public abstract class User {
+    private String id;
+    private String name;
+    private String email;
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    // abstract method - each subclass must define its own display format
+    public abstract void displayInfo();
 }
