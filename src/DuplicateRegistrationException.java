0b0ae4ad71ae8b94f2a7b6b3d8ed2fd9e4ef2e75
Naduli48 <naduli.20232165@iit.ac.lk>
2026-06-21 23:45:37 +0530

    Exception classe added

--- src/DuplicateRegistrationException.java
+++ src/DuplicateRegistrationException.java
@@ -1,6 +1,5 @@
 // Custom checked exception thrown when a student attempts to register with an ID that already exists in the mentorship programme.
-public class DuplicateRegistrationException {
-
+public class DuplicateRegistrationException extends RuntimeException {
     public DuplicateRegistrationException(String message) {
         super(message);
     }
