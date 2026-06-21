cfb9384cba7881d8c6983467077f6488508ce0d6
Naduli48 <naduli.20232165@iit.ac.lk>
2026-06-21 21:42:54 +0530

    Classes added

--- src/Mentor.java
+++ src/Mentor.java
@@ -1,2 +1,37 @@
-public class Mentor {
+public class Mentor extends User {
+
+    private String expertise;
+    private String availability;
+
+    public Mentor(String id, String name, String email,
+                  String expertise, String availability) {
+        super(id, name, email);
+        this.expertise = expertise;
+        this.availability = availability;
+    }
+
+    public String getExpertise() {
+        return expertise;
+    }
+
+    public String getAvailability() {
+        return availability;
+    }
+
+    public void setExpertise(String expertise) {
+        this.expertise = expertise;
+    }
+
+    public void setAvailability(String availability) {
+        this.availability = availability;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Mentor ID    : " + getId());
+        System.out.println("Name         : " + getName());
+        System.out.println("Email        : " + getEmail());
+        System.out.println("Expertise    : " + expertise);
+        System.out.println("Availability : " + availability);
+    }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,34 @@
-public class Student {
+public class Student extends User {
+    private String id;
+    private String name;
+    private String email;
+    private String program;
+    private int year;
+    private String preferences;
+
+    public Student(String id, String name, String email,
+                   String program, int year, String preferences) {
+        super(id, name, email);
+        this.program = program;
+        this.year = year;
+        this.preferences = preferences;
+    }
+
+    public String getProgram() { return program; }
+    public int getYear() { return year; }
+    public String getPreferences() { return preferences; }
+
+    public void setProgram(String program) { this.program = program; }
+    public void setYear(int year) { this.year = year; }
+    public void setPreferences(String preferences) { this.preferences = preferences; }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Student ID   : " + getId());
+        System.out.println("Name         : " + getName());
+        System.out.println("Email        : " + getEmail());
+        System.out.println("Program      : " + program);
+        System.out.println("Year         : " + year);
+        System.out.println("Preferences  : " + preferences);
+    }
 }
