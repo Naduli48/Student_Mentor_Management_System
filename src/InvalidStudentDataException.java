a6dda210be32ab54984c68226d8f205af7062ba8
Naduli48 <naduli.20232165@iit.ac.lk>
2026-07-03 09:05:09 +0530

    made changes

--- src/InvalidStudentDataException.java
+++ src/InvalidStudentDataException.java
@@ -1,5 +1,5 @@
 // Custom checked exception thrown when student registration data is missing, empty, or in an invalid format (e.g. blank name, invalid email).
-public class InvalidStudentDataException extends RuntimeException {
+public class InvalidStudentDataException extends Exception {
     public InvalidStudentDataException(String message) {
         super(message);
     }
